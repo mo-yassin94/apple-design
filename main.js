f4a2fbf2df16ca881d16889802839c4a5fb27869
mo-yassin94 <90253029+mo-yassin94@users.noreply.github.com>
2022-04-08 21:47:48 +0200

    Upload The Design

---
+++ main.js
@@ -0,0 +1,8 @@
+let photo = document.querySelector(".img0");
+let con = document.querySelector(".container");
+function iphone(phone) {
+  photo.src = phone;
+}
+function colors(color) {
+  con.style.background = color;
+}
